5e5a4d6d6b78e6b21b7172aaa4436ec410c921ac
Yevgeniy Valeyev <z.mazay@gmail.com>
2019-09-01 11:45:28 +0200

    Improved FS watcher

--- watcher.go
+++ watcher.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/aws/aws-sdk-go/service/s3"
@@ -18,16 +19,24 @@ func watch(s3Service *s3.S3, timeout time.Duration, site Site, uploadCh chan<- U
 			case event := <-w.Event:
 				if !event.IsDir() {
 					logger.Println(event)
+					// Convert filepath to string
 					filepath := fmt.Sprint(event.Path)
 					if fmt.Sprint(event.Op) == "REMOVE" {
 						s3Key := generateS3Key(site.BucketPath, site.LocalPath, filepath)
 						deleteFile(s3Service, site.Bucket, s3Key)
+					} else if fmt.Sprint(event.Op) == "RENAME" || fmt.Sprint(event.Op) == "MOVE" {
+						// Upload the new object with new name/path
+						uploadCh <- UploadCFG{s3Service, filepath, timeout, site}
+						// remove the old object
+						oldFilepath := fmt.Sprint(event.OldPath)
+						removedS3Key := generateS3Key(site.BucketPath, site.LocalPath, oldFilepath)
+						deleteFile(s3Service, site.Bucket, removedS3Key)
 					} else {
 						excluded := checkIfExcluded(filepath, site.Exclusions)
 						if excluded {
 							logger.Printf("skipping without errors: %+v \n", filepath)
 						} else {
-							uploadCh <- UploadCFG{s3Service, filepath, timeout, site}
+							fileWatcher(s3Service, timeout, site, uploadCh, event, filepath)
 						}
 					}
 				}
@@ -46,5 +55,22 @@ func watch(s3Service *s3.S3, timeout time.Duration, site Site, uploadCh chan<- U
 	// Start the watching process - it'll check for changes every 100ms.
 	if err := w.Start(time.Millisecond * 100); err != nil {
 		logger.Fatal(err)
+	}
+}
+
+// fileWatcher is watching for file mtime and adds the file into the upload queue if it's > 30 seconds old
+// Workaround for - https://github.com/radovskyb/watcher/issues/66
+func fileWatcher(s3Service *s3.S3, timeout time.Duration, site Site, uploadCh chan<- UploadCFG, event watcher.Event, filepath string) {
+	for {
+		select {
+		case <-time.After(time.Second):
+			file, _ := os.Stat(filepath)
+			mtime := file.ModTime()
+			if time.Now().Sub(mtime).Seconds() >= 30 {
+				logger.Printf("there were no writes to the file for 30 seconds, adding to the upload queue: %s", filepath)
+				uploadCh <- UploadCFG{s3Service, filepath, timeout, site}
+				return
+			}
+		}
 	}
 }
