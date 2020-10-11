db69453a51d41e8a768df8fc6c0214ba6d4c1145
Yevgeniy Valeyev <z.mazay@gmail.com>
2020-10-12 00:03:33 +0200

    use proper build dependencies

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -258,8 +258,8 @@ object Release : BuildType({
     params {
         param("teamcity.build.default.checkoutDir", "src/s3sync-service")
         param("env.RELEASE_VERSION", "")
-        param("reverse.dep.S3syncService_Build.RELEASE_VERSION", "env.RELEASE_VERSION")
-        param("reverse.dep.S3syncService_DockerBuild.RELEASE_VERSION", "env.RELEASE_VERSION")
+        param("reverse.dep.S3syncService_Build.RELEASE_VERSION", "")
+        param("reverse.dep.S3syncService_DockerBuild.RELEASE_VERSION", "")
         param("env.RELEASE_CHANGELOG", "")
         param("env.GOPATH", "/opt/buildagent/work")
         checkbox("env.DRAFT_RELEASE", "true",
@@ -280,22 +280,6 @@ object Release : BuildType({
     }
 
     steps {
-        /* script {
-            workingDir = "src"
-            name = "Go get dependencies"
-            scriptContent = "go mod vendor"
-            formatStderrAsError = true
-        }
-        script {
-            name = "Go build"
-            scriptContent = "make build-all"
-            formatStderrAsError = true
-        }
-        script {
-            name = "Docker multi-arch"
-            scriptContent = "make docker-multi-arch"
-            formatStderrAsError = true
-        } */
         script {
             name = "Release"
             scriptContent = """
