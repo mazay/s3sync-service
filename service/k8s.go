b31ce8f425b20cf5c42f856629db4dafa5e2b6c7
Yev <z.mazay@gmail.com>
2024-09-09 20:05:06 +0200

    ResyncPeriod: time.Minute

--- service/k8s.go
+++ service/k8s.go
@@ -77,6 +77,7 @@ func k8sWatchCm(clientset kubernetes.Interface, configmap string, reloaderChan c
 				reloaderChan <- false
 			},
 		},
+		ResyncPeriod: time.Minute,
 	}
 
 	_, controller := cache.NewInformerWithOptions(opts)
